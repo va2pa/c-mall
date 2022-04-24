fed2e76896af29aefc774421fab86df54b6e8c28
sujie <1453648423@qq.com>
2022-04-24 15:41:05 +0800

    编写我的界面

--- pages/my/my.js
+++ pages/my/my.js
@@ -15,52 +15,21 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onMyCoupon(){
+    wx.navigateTo({
+      url: "/pages/my-coupon/my-coupon"
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onMyFavor(){
+    wx.navigateTo({
+      url: "/pages/my-favor/my-favor"
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onMyOrder(){
+    wx.navigateTo({
+      url: "/pages/my-order/my-order?status=0"
+     })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
